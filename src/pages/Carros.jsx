6e405133957cd0928b199e9974ec6b5a55c2cc1a
Pedro <pedrofernandesdelfino@gmail.com>
2024-02-25 13:29:00 -0300

    empty collection return message

--- src/pages/Carros.jsx
+++ src/pages/Carros.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import CarCard from "../components/car-related/CarCard";
 
 function Carros(props) {
+
+    const returnEmptyMessage = () => {
+        return (
+            <p className="text-white text-center"> Você não possui nenhum carro na sua coleção ainda. </p>
+        );
+    }
+
     function handleDelete(index) {
         const newCarCollection = props.carCollection.filter((car, carIndex) => carIndex !== index);
         props.setCarCollection(newCarCollection);
@@ -12,7 +19,7 @@ function Carros(props) {
             <h1 className="text-white text-3xl font-bold text-center mt-10">Sua Coleção</h1>
             <div className="mx-auto text-white mt-10 rounded-xl max-w-3xl bg-neutral-800/30">
                 <div className="px-10 py-10">
-                    {props.carCollection.map((carro, index) => (
+                    {props.carCollection.length === 0 ? returnEmptyMessage() : props.carCollection.map((carro, index) => (
                         <div>
                             <CarCard key={index} carInfo={carro} onDelete={() => handleDelete(index)}/>
                         </div>
