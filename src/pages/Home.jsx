6f83c8fc26b3da7fec80a6d0e8810a46126c18f2
Pedro <pedrofernandesdelfino@gmail.com>
2024-02-25 12:22:51 -0300

    wip: styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function Home(props) {
     return (
-
-        <p> Bem-vindo ao Crud de HotWheels. 
-        </p>
+        <div className="flex flex-col items-center justify-center mt-20">
+            <h1 className="text-4xl font-bold text-white mb-4">Bem-vindo ao CRUD de HotWheels</h1>
+            <p className="text-lg text-white">Aqui você pode gerenciar sua coleção de carros HotWheels de forma simples e fácil.</p>
+            <p className="text-lg text-white mt-2">Entenda mais sobre o projeto em si na aba Sobre, adicione carros na aba Adicionar Carro e explore sua coleção na aba Carros</p>
+        </div>
     );
 }
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 function Sobre(props) {
 
     return (
-        <div className="mx-auto text-white mt-10 rounded-xl max-w-3xl bg-neutral-800/30">
-            <p className="px-10 py-10">
-                Esse é um CRUD de HotWheels desenvolvido durante a disciplina de Desenvolvimento Front-End do curso de
-                Análise e Desenvolvimento de Sistemas
-                da Pontifícia Universidade Católica do Rio Grande do Sul.
-            </p>
+        <div className="mx-auto mt-10 rounded-xl max-w-3xl bg-neutral-800/30">
+            <div className="text-white p-8">
+                <h2 className="text-2xl font-bold mb-4">Sobre este projeto</h2>
+                <p className="text-lg">
+                    Este é um aplicativo CRUD (Create, Read, Update, Delete) desenvolvido durante a disciplina de Desenvolvimento Front-End do curso de Análise e Desenvolvimento de Sistemas da Pontifícia Universidade Católica do Rio Grande do Sul.
+                </p>
+                <p className="text-lg mt-4">
+                    O objetivo deste projeto é permitir aos usuários gerenciar sua coleção de carros HotWheels de forma simples e eficiente.
+                </p>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Sobre;
